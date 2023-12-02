a25b9c6f01967becc08e589979f6c1ead0b483b8
Antonio Gomez Yunta <antonio.gomez.yunta@bbva.com>
2023-12-02 16:05:03 +0100

    boton mostrar

--- scripts.js
+++ scripts.js
@@ -21,6 +21,38 @@ function mostrarProfesion(carta) {
   }
 }
 
+let mostrandoReversos = false;
+
+function alternarCartas() {
+  const cartas = document.querySelectorAll('.carta');
+  const titulo = document.getElementById('titulo');
+  const btn = document.getElementById('alternarBtn');
+
+  if (!mostrandoReversos) {
+    // Mostrar los reversos
+    cartas.forEach(carta => {
+      const frontal = carta.querySelector('.frontal');
+      const trasera = carta.querySelector('.trasera');
+      frontal.style.transform = 'rotateY(-180deg)';
+      trasera.style.transform = 'rotateY(0deg)';
+    });
+    titulo.textContent = 'Descubre los amigos';
+    btn.textContent = 'Mostrar Amigos';
+    mostrandoReversos = true;
+  } else {
+    // Mostrar los anversos
+    cartas.forEach(carta => {
+      const frontal = carta.querySelector('.frontal');
+      const trasera = carta.querySelector('.trasera');
+      frontal.style.transform = 'rotateY(0deg)';
+      trasera.style.transform = 'rotateY(180deg)';
+    });
+    titulo.textContent = 'Descubre las Profesiones';
+    btn.textContent = 'Mostrar Profesiones';
+    mostrandoReversos = false;
+  }
+}
+
 
 function moverImagen() {
   const imagen = document.querySelector('.imagen-conductora');
